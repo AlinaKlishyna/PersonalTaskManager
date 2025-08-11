41bd46cb3ad403a8c2be7c3e9fc949f7ae94d626
Alina <alinakotova102@gmail.com>
2025-08-11 11:34:00 +0200

    added class TaskDAO com methods CRUD

---
+++ src/com/gmail/alinakotova102/dao/task/TaskDAO.java
@@ -0,0 +1,9 @@
+package com.gmail.alinakotova102.dao.task;
+
+import com.gmail.alinakotova102.dao.DAO;
+import com.gmail.alinakotova102.model.Task;
+
+public interface TaskDAO extends DAO<Task> {
+    Task findByName(String name);
+    Task findAuthor(String author);
+}
