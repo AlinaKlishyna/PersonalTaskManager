35e45f98349eb8c22594a292269853cfc23131ca
Alina <alinakotova102@gmail.com>
2025-08-11 11:34:47 +0200

    added class with realization TaskDAOFile for file

---
+++ src/com/gmail/alinakotova102/dao/task/TaskDAOFile.java
@@ -0,0 +1,68 @@
+package com.gmail.alinakotova102.dao.task;
+
+import com.gmail.alinakotova102.model.Task;
+
+import java.util.List;
+
+public class TaskDAOFile implements TaskDAO{
+    private static TaskDAOFile uniqueInstance;
+
+    private TaskDAOFile() {}
+
+    public static TaskDAOFile getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new TaskDAOFile();
+        }
+        return uniqueInstance;
+    }
+
+    @Override
+    public void insert(Task task) {
+        
+    }
+
+    @Override
+    public String read(String path) {
+        return null;
+    }
+
+    @Override
+    public void update(Task task) {
+
+    }
+
+    @Override
+    public void delete(Task task) {
+
+    }
+
+    @Override
+    public void display(Task task) {
+
+    }
+
+    @Override
+    public void displayAll() {
+
+    }
+
+    @Override
+    public Task get(Task task) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getAll() {
+        return null;
+    }
+
+    @Override
+    public Task findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Task findAuthor(String author) {
+        return null;
+    }
+}
