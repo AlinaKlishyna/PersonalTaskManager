586da806912a19339bd705842e418fdc10fdf102
Alina <alinakotova102@gmail.com>
2025-08-12 17:53:37 +0200

    added classes with business logic

--- src/com/gmail/alinakotova102/dao/task/TaskDAO.java
+++ src/com/gmail/alinakotova102/dao/task/TaskDAO.java
@@ -3,8 +3,13 @@ package com.gmail.alinakotova102.dao.task;
 import com.gmail.alinakotova102.dao.DAO;
 import com.gmail.alinakotova102.model.Task;
 
-public interface TaskDAO extends DAO<Task> {
-    public Task findByName(String name);
+import java.util.List;
 
-    public Task findAuthor(String author);
+public interface TaskDAO extends DAO<Task> {
+    public void displaySortName(List<Task> task);
+    public void displaySortLastName(List<Task> task);
+    public void displaySortCreationDate(List<Task> task);
+    public void displaySortExecutionDate(List<Task> task);
+    public void editTask(Task task);
+    public void completeMark(Task task);
 }
