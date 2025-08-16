22075d06d55e91e5312c99160ea390c323153107
Alina <alinakotova102@gmail.com>
2025-08-16 14:04:37 +0200

    modified class exception

--- src/com/gmail/alinakotova102/exception/NotFoundException.java
+++ src/com/gmail/alinakotova102/exception/NotFoundException.java
@@ -3,7 +3,15 @@ package com.gmail.alinakotova102.exception;
 import com.gmail.alinakotova102.util.DisplayUtil;
 
 public class NotFoundException extends Exception {
+    public NotFoundException() {
+    }
+
     public NotFoundException(Object o) {
-        DisplayUtil.console("I couldn't find the object : " + o + ";");
+        DisplayUtil.view("I couldn't find the object : " + o + ";");
+    }
+
+    @Override
+    public String getMessage() {
+        return "Ops! I don't found :(";
     }
 }
