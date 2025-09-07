eca5fd7e20b8d5e41c44060b5f740ef0ff5824af
Alina <alinakotova102@gmail.com>
2025-09-07 18:00:06 +0200

    update class Runner with start point

--- src/com/gmail/alinakotova102/Runner.java
+++ src/com/gmail/alinakotova102/Runner.java
@@ -1,5 +1,7 @@
 package com.gmail.alinakotova102;
 
+import com.gmail.alinakotova102.exception.DataException;
+import com.gmail.alinakotova102.exception.DateTaskException;
 import com.gmail.alinakotova102.exception.NotFoundException;
 import com.gmail.alinakotova102.model.Person;
 import com.gmail.alinakotova102.model.Priority;
@@ -7,14 +9,16 @@ import com.gmail.alinakotova102.model.Status;
 import com.gmail.alinakotova102.model.Task;
 import com.gmail.alinakotova102.serialization.PersonSerializer;
 import com.gmail.alinakotova102.serialization.TaskSerializer;
+import com.gmail.alinakotova102.service.PersonService;
+import com.gmail.alinakotova102.service.TaskService;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Runner {
-    public static void main(String[] args) throws NotFoundException {
-        Person p1 = new Person("Владислав", "Лисяк", "doublefmove@gmail.com");
+    public static void main(String[] args) throws NotFoundException, DataException, DateTaskException {
+        Person p1 = new Person("Владислав", "Лис", "doublefmove@gmail.com");
         Person p2 = new Person("Алина", "Клишина", "alinakotova102@gmail.com");
         Person p3 = new Person("Василиса", "Бермудов", "vasilisa200@gmail.com");
         Person p4 = new Person("Василий", "Славко", "vas2020@gmail.com");
@@ -26,16 +30,17 @@ public class Runner {
 
         ArrayList<Person> personsDes = PersonSerializer.deserialize("resources/file/persons.txt");
 
+        System.out.println("\nTesting.. Deserializer Persons -------------->");
         for (Person person : personsDes) {
             System.out.println(person);
         }
 
         Task t1 = new Task(
                 "Task1",
-                null,
-                Priority.FAST,
+                "null",
+                Priority.URGENT,
                 LocalDate.now(),
-                LocalDate.of(2025, 6, 12),
+                LocalDate.of(2026, 12, 12),
                 p1,
                 p1,
                 Status.PROCESS);
@@ -63,7 +68,25 @@ public class Runner {
         TaskSerializer.serialize(tasks, "resources/file/tasks.txt");
         ArrayList<Task> tasksBySerializer = TaskSerializer.deserialize("resources/file/tasks.txt");
 
+        System.out.println("\nTesting.. Deserialize Tasks -------------->");
         for (Task task : tasksBySerializer) {
+            System.out.println(task);
+        }
+
+        System.out.println("\nTesting.. PersonService -------------->");
+        PersonService personService = PersonService.getInstance();
+        for (Person person : persons) {
+            personService.addPerson(person);
+        }
+
+        System.out.println("\nTesting.. TaskService -------------->");
+        TaskService taskService = TaskService.getInstance();
+        for (Task task : tasks) {
+            taskService.addTask(task);
+        }
+
+        List<Task> tasksByDAO = taskService.viewTasks();
+        for (Task task : tasksByDAO) {
             System.out.println(task);
         }
     }
